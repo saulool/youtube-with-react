8bb65a527086b18b94534ad034e30591d58a14a5
Saulo Antunes Oliveira <santunes@dimed.local>
2017-02-16 13:12:23 -0200

    Fixed search to only search for videos, added channel title info, fixed duplicated suggestion

--- src/components/videoDescription.js
+++ src/components/videoDescription.js
@@ -4,6 +4,7 @@ const VideoDescription = (props) => {
 	return (
 		<div className="description">
 			<h1 className="title">{props.title}</h1>
+			<h2 className="author">{props.author}</h2>
 			<p className="about">{props.description}</p>
 		</div>
 	);
